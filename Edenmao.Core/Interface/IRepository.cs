463ccbeaabbc0eef387a4f8963fbdf33cda5082a
Orlando Abreu <109491615+OrlandoNK@users.noreply.github.com>
2024-10-05 19:26:38 -0400

    referencias y irepository

--- Edenmao.Core/Interface/IRepository.cs
+++ Edenmao.Core/Interface/IRepository.cs
@@ -6,7 +6,12 @@ using System.Threading.Tasks;
 
 namespace Edenmao.Core.Interface
 {
-    internal class IRepository
+    public interface IRepository<T> where T : class
     {
+        Task<T> GetByIdAsync(int id);
+        Task<IEnumerable<T>> GetAllAsync();
+        Task AddAsync(T entity);
+        Task UpdateAsync(T entity);
+        Task DeleteAsync(int id);
     }
 }
